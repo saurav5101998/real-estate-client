293719f0e17a9a2b9fcc4aec2454977f5ced5b0a
saurav5101998 <sauravlpu11@gmail.com>
2024-05-15 02:30:46 +0530

    Ecommerce mern app With Tailwind Css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      {" "}
+      <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+        Find your next <span className="text-slate-500">perfect</span>
+        <br />
+        place with ease
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
